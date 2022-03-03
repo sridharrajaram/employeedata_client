e4fe59ea896dd61c8dbd662d97f99071a118cd72
Sridhar Rajaram <srilumia@hotmail.com>
2022-03-03 12:14:30 +0530

    table data updated

--- src/components/Department/Department.js
+++ src/components/Department/Department.js
@@ -40,33 +40,35 @@ function Department() {
           </Button>
         </Link>
       </Row>
-      <Row className="mt-5">
-        <Card>
-          <Card.Body>
-            <Card.Header>
-              <b>Department Details</b>
-            </Card.Header>
-            <Table bordered hover variant="success">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Department Name</th>
-                </tr>
-              </thead>
-              <tbody>
-                {departmentList.map((obj, Index) => {
-                  return (
-                    <tr key={Index + 1}>
-                      <td>{Index + 1}</td>
-                      <td>{obj.departmentName}</td>                      
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </Card.Body>
-        </Card>
-      </Row>
+      {departmentList.length === 0 ? null : (
+        <Row className="mt-5">
+          <Card>
+            <Card.Body>
+              <Card.Header>
+                <b>Department Details</b>
+              </Card.Header>
+              <Table bordered hover variant="success">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Department Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {departmentList.map((obj, Index) => {
+                    return (
+                      <tr key={Index + 1}>
+                        <td>{Index + 1}</td>
+                        <td>{obj.departmentName}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </Row>
+      )}
     </Container>
   );
 }
