21ad64d3b4b8cceea8079300bd9bfb55fce6b2ea
Sridhar Rajaram <srilumia@hotmail.com>
2022-03-03 12:05:52 +0530

    Heroku Url appended

--- src/components/Config/UrlSettings.js
+++ src/components/Config/UrlSettings.js
@@ -1,6 +1,7 @@
 const getUrlSettings = () => {
     return {
-        api:"http://localhost:5000",
+        api1:"http://localhost:5000",
+        api:"https://sridharrajaram-empdata-server.herokuapp.com",
         GEOAPIFY_APIKEY:"56b0b630cd9b45d98b09dca9ebdd8ca8"
     }
 }
